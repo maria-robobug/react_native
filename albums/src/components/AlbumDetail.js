478896301fbb94ba1166ea7640f34754e1676e0c
maria-robobug <mariashow2001@gmail.com>
2018-08-28 14:26:38 +0100

    Added Card section

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnail,
+    header,
+    thumbnailContainer,
+    headerText,
+    albumImage
+  } = styles;
+
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainer}>
+          <Image
+            style={thumbnail}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={header}>
+          <Text style={headerText}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={albumImage}
+          source={{ uri: image }}
+        />
+      </CardSection>
+    </Card>
   );
+};
+
+const styles = {
+  header: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerText: {
+    fontSize: 18
+  },
+  thumbnail: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumImage: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
